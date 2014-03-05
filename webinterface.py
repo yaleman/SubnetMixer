92a382365eb7ec22cc8503ece70e3f72f1ddde52
James Hodgkinson <yaleman@ricetek.net>
2014-03-05 12:53:22 +1000

    initial web interface testing

---
+++ webinterface.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from subnet import Subnet
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route( "/" )
+def index():
+	retval = "<pre>"
+	for (n,b) in [ ("255.255.255.1", 24), ("192.168.0.2", 25), ("0.0.0.0", 0 ), ("10.2.3.4", 32 ) ]:
+		subnet = Subnet( n, b )
+
+		retval +=  "#" * 50 + "\n"
+		retval += str( subnet )+"\n"
+		try:
+			retval += str( subnet.validate() )+"\n"
+		except subnet.SubnetException, e:
+			retval += "{}\n".format( str( e ) )
+		#retval += "\ntesting ipv4toint\n"
+		#retval += "{}\n".format( str( subnet.ipv4toint( subnet.address ) ) )
+		retval += "dumping binary\n"
+		retval += "{}\n".format( str(  subnet.binarydump() ) ) 
+	return retval
+
+
+if __name__ == "__main__":
+	app.run()
